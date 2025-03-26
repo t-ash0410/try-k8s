36c9daa9cfc696e50a36922a9bf61918d13b4c98
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-03-26 12:24:51 +0900

    bundle bootstrap

--- k8s/argocd/bootstrap/setup.sh
+++ k8s/argocd/bootstrap/setup.sh
@@ -1,9 +1,51 @@
-# /bin/bash
+#!/bin/bash
 
+LOCAL_PORT=8081
+
+######################### Start Minikube #########################
+
+# If an error occurs, you might want to check this issue.
+# https://github.com/kubernetes/minikube/issues/12274#issuecomment-1006201978
 minikube start
+
+######################### Setup ArgoCD #########################
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "ArgoCD admin password:"
-kubectl -n argocd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+######################### Port Forwarding #########################
+
+local argocd_server=$(kubectl get replicaset -n argocd | grep argocd-server | awk '{print $1}')
+
+./wait-for-replicaset.sh $argocd_server argocd
+
+kubectl port-forward -n argocd replicaset/$argocd_server $LOCAL_PORT:8080 &
+pf=$!
+
+for ((i=0 ; i<20 ; i++)); do
+    sleep 0.1
+    if [ `ps -p $pf -o state | tail -n 1` != "S+" ]; then
+        continue
+    fi
+done
+
+######################### Get Admin Password #########################
+
+local admin_secret=$(kubectl -n argocd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo "ArgoCD admin password: $admin_secret"
+
+######################### Login #########################
+
+argocd login --insecure localhost:8081
+
+######################### Add Repository #########################
+
+argocd repo add https://github.com/t-ash0410/try-k8s.git \
+  --username $GITHUB_USER \
+  --password $GITHUB_PAT
+
+######################### Apply Bootstrap #########################
+
+kubectl apply -f ./argocd.yaml
+
+kill $pf
